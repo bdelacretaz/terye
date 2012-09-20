afe47bbcaec383e2fe1bc6a8001615a1ad764c3a
Damien Obrist <damien.obrist@gmail.com>
2012-09-20 10:55:37 +0200

    new node tests

--- terye-firsthops/src/test/java/ch/x42/terye/NodeTest.java
+++ terye-firsthops/src/test/java/ch/x42/terye/NodeTest.java
@@ -2,7 +2,9 @@ package ch.x42.terye;
 
 import static org.junit.Assert.assertEquals;
 
+import javax.jcr.ItemExistsException;
 import javax.jcr.Node;
+import javax.jcr.PathNotFoundException;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 
@@ -13,10 +15,12 @@ import org.junit.Test;
 public class NodeTest {
 
     private Session session;
+    private Node root;
 
     @Before
     public void setup() throws RepositoryException {
         session = new RepositoryImpl().login();
+        root = session.getRootNode();
     }
 
     @After
@@ -28,10 +32,32 @@ public class NodeTest {
 
     @Test
     public void testAdd() throws RepositoryException {
-        Node rootNode = session.getRootNode();
-        Node a1 = rootNode.addNode("a");
-        assertEquals("a", a1.getName());
-        assertEquals("/a", a1.getPath());
+        Node a = root.addNode("a");
+        assertEquals("a", a.getName());
+        assertEquals("/a", a.getPath());
+        
+        Node b = root.addNode("a/b");
+        assertEquals("b", b.getName());
+        assertEquals("/a/b", b.getPath());
+        
+        Node c = b.addNode("c");
+        assertEquals("c", c.getName());
+        assertEquals("/a/b/c", c.getPath());
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void testAddIllegalArgument() throws RepositoryException {
+        root.addNode("/not/allowed");
+    }
+    
+    @Test(expected=PathNotFoundException.class)
+    public void testAddPathNotFound() throws RepositoryException {
+        root.addNode("leads/to/nowhere");
     }
 
+    @Test(expected=ItemExistsException.class)
+    public void testAddItemExists() throws RepositoryException {
+        root.addNode("a");
+        root.addNode("a");
+    }
 }
