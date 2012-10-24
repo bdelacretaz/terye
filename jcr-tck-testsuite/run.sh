7567738ab3941c6a3394dc8e325d61eada451aca
Damien Obrist <damien.obrist@gmail.com>
2012-10-24 11:34:14 +0200

    INT-25: implemented test suite filter

--- jcr-tck-testsuite/run.sh
+++ jcr-tck-testsuite/run.sh
@@ -1,14 +1,45 @@
 #!/bin/bash
 
+function show_usage {
+    echo "Usage: ./run.sh [-a] [-t] [terye_artifact]"
+    echo "  -a: build artifact"
+    echo "  -t: build test suite"
+	exit 1
+}
+
+build_artifact=false
+build_testsuite=false
+
+# parse options
+while getopts ":at" opt; do
+	case $opt in
+		a)
+			build_artifact=true
+		;;
+		t)
+			build_testsuite=true
+		;;
+		\?)
+			show_usage
+		;;
+	esac
+done
+
+# skip options
+shift $(( OPTIND -1 ))
+
 # make sure we're in the script's folder
 cd $(dirname $0)
 dir=`pwd`
 
+# verify argument and corresponding folder exist
 if [ $# -eq 0 -o ! -d ../$1 ]; then
-    echo "Usage: ./run.sh [terye_artifact]"
+	show_usage
 else
-	# compile jcr-tck-testsuite
-	mvn compile
+	if $build_testsuite ; then
+		# compile jcr-tck-testsuite
+		mvn compile
+	fi
 	# add class files to classpath
 	classpath=./target/classes
 	# generate dependencies classpath of jcr-tck-testsuite...
@@ -16,8 +47,10 @@ else
 	# ... and add it to the classpath
 	classpath=$classpath:`cat .cp`
 
-	# package (i.e. compile etc) the artifact to be tested
-	mvn -f ../$1/pom.xml package
+	if $build_artifact ; then
+		# package (i.e. compile etc) the artifact to be tested
+		mvn -f ../$1/pom.xml package
+	fi
 	# add jar of the artifact to be tested
 	classpath=$classpath:../$1/target/$1-1.0-SNAPSHOT.jar
 	# generate the artifact's dependencies classpath...
--- jcr-tck-testsuite/src/main/java/ch/x42/terye/test/FilteredTestSuite.java
+++ jcr-tck-testsuite/src/main/java/ch/x42/terye/test/FilteredTestSuite.java
@@ -1,5 +1,15 @@
 package ch.x42.terye.test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 import org.apache.jackrabbit.test.ConcurrentTestSuite;
@@ -7,11 +17,68 @@ import org.apache.jackrabbit.test.JCRTestSuite;
 
 public class FilteredTestSuite {
 
-    public static TestSuite suite() {
-        TestSuite jcrTestSuite = new JCRTestSuite();
-        ConcurrentTestSuite suite = new ConcurrentTestSuite(jcrTestSuite.getName());
-        
-        return jcrTestSuite;
+    private static SortedSet<String> excludedPrefixes = new TreeSet<String>();
+
+    public static TestSuite suite() throws IOException {
+        // read file of excluded prefixes
+        InputStream is = FilteredTestSuite.class
+                .getResourceAsStream("/exclude.txt");
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        String line;
+        while ((line = br.readLine()) != null) {
+            String prefix = line.trim();
+            if (!prefix.isEmpty()) {
+                excludedPrefixes.add(prefix);
+            }
+        }
+
+        // create original test suite containing all tests
+        TestSuite suite = new JCRTestSuite();
+        // create filtered test suite to be returned
+        ConcurrentTestSuite filteredSuite = new ConcurrentTestSuite(
+                suite.getName());
+
+        // nested-loop through all tests
+        Enumeration enum1 = suite.tests();
+        while (enum1.hasMoreElements()) {
+            TestSuite suite1 = (TestSuite) enum1.nextElement();
+            TestSuite filteredSuite1 = new TestSuite(suite1.getName());
+            Enumeration enum2 = suite1.tests();
+            while (enum2.hasMoreElements()) {
+                TestSuite suite2 = (TestSuite) enum2.nextElement();
+                TestSuite filteredSuite2 = new TestSuite(suite2.getName());
+                Enumeration enum3 = suite2.tests();
+                while (enum3.hasMoreElements()) {
+                    // here we are at test level
+                    TestCase test = (TestCase) enum3.nextElement();
+                    String fullName = suite2.getName() + "." + test.getName();
+                    if (!isExcluded(fullName)) {
+                        filteredSuite2.addTest(test);
+                    }
+                }
+                // only add sub suite if it contains some tests
+                if (filteredSuite2.countTestCases() > 0) {
+                    filteredSuite1.addTest(filteredSuite2);
+                }
+            }
+            // only add sub suite if it contains some tests
+            if (filteredSuite1.countTestCases() > 0) {
+                filteredSuite.addTest(filteredSuite1);
+            }
+        }
+
+        return filteredSuite;
+    }
+
+    private static boolean isExcluded(String name) {
+        Iterator<String> iterator = excludedPrefixes.iterator();
+        while (iterator.hasNext()) {
+            String prefix = iterator.next();
+            if (name.startsWith(prefix)) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
