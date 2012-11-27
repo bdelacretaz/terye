2d0ab11ec581a8a6d5885afd921dba73f7aec76d
Damien Obrist <damien.obrist@gmail.com>
2012-11-27 09:50:36 +0100

    INT-49: make dispatch asynchronous

--- hbase-firsthops/sling-hbase-repository/src/main/java/ch/x42/terye/observation/ObservationDispatcher.java
+++ hbase-firsthops/sling-hbase-repository/src/main/java/ch/x42/terye/observation/ObservationDispatcher.java
@@ -3,13 +3,26 @@ package ch.x42.terye.observation;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
-public class ObservationDispatcher {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class ObservationDispatcher implements Runnable {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    private Thread thread;
+    private BlockingQueue<EventCollection> queue;
     private Set<EventConsumer> consumers;
 
     protected ObservationDispatcher() {
-        this.consumers = new HashSet<EventConsumer>();
+        thread = new Thread(this, "ObservationManager");
+        thread.setDaemon(true);
+        queue = new LinkedBlockingQueue<EventCollection>();
+        consumers = new HashSet<EventConsumer>();
+        thread.start();
     }
 
     protected void addConsumer(EventConsumer consumer) {
@@ -21,13 +34,33 @@ public class ObservationDispatcher {
     }
 
     protected void dispatchEvents(EventCollection events) {
-        for (EventConsumer consumer : consumers) {
-            consumer.consume(events);
+        try {
+            queue.put(events);
+        } catch (InterruptedException e) {
+            logger.warn("Interrupted while putting to event queue");
         }
     }
 
     protected Set<EventConsumer> getConsumers() {
         return Collections.unmodifiableSet(consumers);
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                EventCollection events = queue.take();
+                for (EventConsumer consumer : consumers) {
+                    try {
+                        consumer.consume(events);
+                    } catch (Throwable e) {
+                        logger.warn("Consumer threw an exception");
+                    }
+                }
+            } catch (InterruptedException e) {
+                logger.warn("Interrupted while taking from event queue");
+            }
+        }
     }
 
 }
