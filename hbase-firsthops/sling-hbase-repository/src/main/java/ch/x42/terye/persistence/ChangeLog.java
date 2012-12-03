04b0a2d88982163ec807ddef5e33dea1a7bcaec9
Damien Obrist <damien.obrist@gmail.com>
2012-12-03 17:04:18 +0100

    INT-54: added useful method to changelog

--- hbase-firsthops/sling-hbase-repository/src/main/java/ch/x42/terye/persistence/ChangeLog.java
+++ hbase-firsthops/sling-hbase-repository/src/main/java/ch/x42/terye/persistence/ChangeLog.java
@@ -1,10 +1,18 @@
 package ch.x42.terye.persistence;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
+import javax.jcr.RepositoryException;
+
 import ch.x42.terye.ItemImpl;
+import ch.x42.terye.path.Path;
+import ch.x42.terye.path.PathFactory;
 import ch.x42.terye.persistence.id.ItemId;
 
 public class ChangeLog {
@@ -40,6 +48,56 @@ public class ChangeLog {
 
     public Collection<ItemState> getRemovedStates() {
         return removedStates.values();
+    }
+
+    /**
+     * This method returns the minimal subset of the removed states, such that
+     * every removed state is either part of the returned set or a descendant of
+     * one of the states in the returned set.
+     */
+    public List<ItemState> getMinimalRemovedStates() throws RepositoryException {
+        // states contains all removed states
+        List<ItemState> states = new LinkedList<ItemState>(
+                removedStates.values());
+        // sort states by increasing depth
+        Collections.sort(states, new Comparator<ItemState>() {
+
+            @Override
+            public int compare(ItemState s1, ItemState s2) {
+                Path p1 = PathFactory.create(s1.getPath());
+                Path p2 = PathFactory.create(s2.getPath());
+                if (p1.getDepth() < p2.getDepth()) {
+                    return -1;
+                } else if (p1.getDepth() == p2.getDepth()) {
+                    return 0;
+                }
+                return 1;
+            }
+
+        });
+        // start with an empty list of minimal roots
+        List<ItemState> roots = new LinkedList<ItemState>();
+        // loop through all states
+        for (ItemState state : states) {
+            boolean covered = false;
+            // loop through all minimal roots
+            for (ItemState root : roots) {
+                Path statePath = PathFactory.create(state.getPath());
+                Path rootPath = PathFactory.create(root.getPath());
+                // if this state is a descendant of one of the minimal roots, we
+                // can discard it
+                if (statePath.isDescendantOf(rootPath)) {
+                    covered = true;
+                    break;
+                }
+            }
+            // this state is not a descendant of one of the minimal roots, add
+            // to minimal roots
+            if (!covered) {
+                roots.add(state);
+            }
+        }
+        return roots;
     }
 
     public boolean isEmpty() {
