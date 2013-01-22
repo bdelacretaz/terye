a303cdf85a698d0bd2380eca85817102c987b40a
Damien Obrist <damien.obrist@gmail.com>
2013-01-22 14:36:10 +0100

    INT-73: method for generating new revision ids

--- oak-mk-hbase/src/main/java/ch/x42/terye/mk/hbase/HBaseMicroKernel.java
+++ oak-mk-hbase/src/main/java/ch/x42/terye/mk/hbase/HBaseMicroKernel.java
@@ -1,6 +1,9 @@
 package ch.x42.terye.mk.hbase;
 
 import java.io.InputStream;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Random;
 
 import org.apache.hadoop.hbase.client.HBaseAdmin;
 import org.apache.jackrabbit.mk.api.MicroKernel;
@@ -107,6 +110,65 @@ public class HBaseMicroKernel implements MicroKernel {
     public String write(InputStream in) throws MicroKernelException {
         // TODO Auto-generated method stub
         return null;
+    }
+
+    /* private methods */
+
+    /**
+     * This method generates a new revision id. A revision id is a signed (but
+     * non-negative) long composed of the concatenation (left-to-right) of
+     * following fields:
+     * 
+     * <pre>
+     *  --------------------------------------------------------------
+     * |                    timestamp |    machine_id |       counter |
+     *  --------------------------------------------------------------
+     *  
+     * - timestamp: signed int, 4 bytes, [0, Integer.MAX_VALUE]
+     * - machine_id: unsigned short, 2 bytes, [0, 65535]
+     * - counter: unsigned short, 2 bytes, [0, 65535]
+     * </pre>
+     * 
+     * The unit of the timestamp is seconds and since we only have 4 bytes
+     * available, we base it on an artificial epoch (defined in the method) in
+     * order to have more values available. The machine id is generated using
+     * the hashcode of the MAC address string and not guaranteed to be unique
+     * (but very likely so). If the same machine commits a revision within the
+     * same second, then the counter field is used in order to create a unique
+     * revision id.
+     * 
+     * @return a new and unique revision id
+     */
+    public long generateNewRevisionId() {
+        // NEVER EVER CHANGE THIS VALUE
+        final long epoch = 1358845000;
+
+        // timestamp
+        long seconds = System.currentTimeMillis() / 1000 - epoch;
+        long timestamp = seconds << 32;
+
+        // machine id
+        long machineId;
+        try {
+            NetworkInterface network = NetworkInterface
+                    .getByInetAddress(InetAddress.getLocalHost());
+            byte[] address = network.getHardwareAddress();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < address.length; i++) {
+                sb.append(String.format("%02X", address[i]));
+            }
+            machineId = sb.hashCode();
+        } catch (Throwable e) {
+            machineId = new Random().nextInt();
+        }
+        machineId = (machineId << 16) & Long.decode("0x00000000FFFF0000");
+
+        // counter
+        // XXX: tbd
+        long counter = 0;
+
+        // assemble and return revision id
+        return timestamp | machineId | counter;
     }
 
 }
